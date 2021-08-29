302f6b3c5c3f33624004f58a3f9451c9b6b06875
mkrela <77677471+mkrela@users.noreply.github.com>
2021-08-29 18:16:51 +0200

    Essai

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,50 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const helmet = require('helmet');
+const path = require("path");
+require('dotenv').config();
+
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauces');
+
+mongoose.connect(
+    
+)
 
 const app = express();
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('./api/sauces',(req,res, next) => {
+    const sauces = [
+        {
+            _id: 'sauce1',
+            title: 'amora',
+            description: 'de la bonne mayo',
+            imageUrl: '',
+            price: 5000,
+            userId: 'ola',
+        },
+        {
+            _id: 'sauce2',
+            title: 'ama',
+            description: 'de la mayo',
+            imageUrl: '',
+            price: 500,
+            userId: 'ola2',
+        },
+    ];
+    console.log('allooooofoooo')
+    res.status(200).json(sauces);
+});
+
+app.use((req, res, next) => {
+   res.json({ message: 'Votre requête a bien été r!' }); 
 });
 
 module.exports = app;
