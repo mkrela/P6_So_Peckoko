e1d13417aac3df91f32dd8c768dfa4a75b4bd7c3
mkrela <77677471+mkrela@users.noreply.github.com>
2021-08-30 15:35:00 +0200

    Ajout de delete

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -6,20 +6,40 @@ exports.getAllSauces = (req, res, next) => {
     .catch((error) => res.status(404).json({ error }));
 };
 
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+};
+
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
   const sauce = new Sauce({
     ...req.body,
     ...sauceObject,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
     likes: 0,
     dislikes: 0,
-    usersLiked: [' '],
-    usersdisLiked: [' '],
+    usersLiked: [" "],
+    usersdisLiked: [" "],
   });
   sauce
     .save()
     .then(() => res.status(201).json({ message: "Objet enregistré" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.updateOne = (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(res.status(200).json({ message: "Sauce modifiée" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.deleteOne = (req, res, next) => {
+  Sauce.deleteOne({ _id: req.params.id })
+    .then(res.status(200).json({ message: "Objet supprimé" }))
     .catch((error) => res.status(400).json({ error }));
 };
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const app = require('../app');
-const router = express.Router()
-const multer= require ('../middleware/multer-config')
-const path = require ('path')
+const app = require("../app");
+const router = express.Router();
+const multer = require("../middleware/multer-config");
+const path = require("path");
 
-const sauceCtrl = require('../controllers/sauce')
+const sauceCtrl = require("../controllers/sauce");
 
-router.get('/', sauceCtrl.getAllSauces)
-// router.get("/:id", sauceCtrl.XXX)
-router.post("/", multer, sauceCtrl.createSauce)
-// router.put("/:id", sauceCtrl.XXX)
-// router.delete("/:id", sauceCtrl.XXX)
+router.get("/", sauceCtrl.getAllSauces);
+router.get('/:id', sauceCtrl.getOneSauce);
+router.post("/", multer, sauceCtrl.createSauce);
+router.put("/:id", sauceCtrl.updateOne)
+router.delete("/:id", sauceCtrl.deleteOne);
 
-module.exports = router 
+module.exports = router;
