4aea4283fe83d1fecebdd69dd2dd160f9f4e85d1
mkrela <77677471+mkrela@users.noreply.github.com>
2021-08-31 14:21:21 +0200

    Backup

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use("/images", express.static(path.join(__dirname, "images")));
-app.use("/api/auth", userRoutes);
 app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
