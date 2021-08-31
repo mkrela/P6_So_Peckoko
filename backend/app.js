e0e5c35093b282712ccdb40f2ccddbdfcb1d54e6
mkrela <77677471+mkrela@users.noreply.github.com>
2021-08-31 12:18:19 +0200

    Dernier commit avant changement de controllers

--- backend/app.js
+++ backend/app.js
@@ -6,16 +6,18 @@ const sauceRoutes = require("./routes/sauces");
 const path = require("path");
 const Sauce = require("./models/Sauce.js");
 
+const app = express();
+
+// Connexion à la base de donnée MongoDB -------------------------
 mongoose
   .connect(
     "mongodb+srv://mkrela:mattfado57@projet6.ldg9u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true, 
-      useUnifiedTopology: true }
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-const app = express();
+//  Résolution du problème de CORS -------------------------------
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -30,45 +32,41 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use((req, res, next) => {
-//   res.json({ message: "yep" });
-//   next();
-// });
-
 app.use(bodyParser.json());
 
 app.post("http://localhost:3000/api/sauces", (req, res, next) => {
   delete req.body._id;
   const sauce = new Sauce({
-    ...req.body
+    ...req.body,
   });
-  sauce.save()
-  .then(() => res.status(201).json({ message: "Sauce enregistrée !"}))
-  .catch(error => res.status(400).json({ error }));
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Sauce enregistrée !" }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
-app.put('/api/sauces/:id', (req, res, next) => {
+app.put("/api/sauces/:id", (req, res, next) => {
   Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-    .catch(error => res.status(400).json({ error }));
+    .then(() => res.status(200).json({ message: "Sauce modifiée !" }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
-app.delete('/api/sauces/:id', (req, res, next) => {
+app.delete("/api/sauces/:id", (req, res, next) => {
   Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-    .catch(error => res.status(400).json({ error }));
+    .then(() => res.status(200).json({ message: "Sauce supprimée !" }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
-app.get('/api/sauces/:id', (req, res, next) => {
+app.get("/api/sauces/:id", (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }));
 });
 
-app.get('/api/sauces', (req, res, next) => {
- Sauce.find()
- .then(sauces => res.status(200).json(sauces))
- .catch(error => res.status(404).json({ error }));
+app.get("/api/sauces", (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(404).json({ error }));
 });
 
 app.use("/api/auth", userRoutes);
