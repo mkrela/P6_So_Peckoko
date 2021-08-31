325c45ca04c5de49a5bb9bfc6dd2f2e9a868e962
mkrela <77677471+mkrela@users.noreply.github.com>
2021-08-31 12:32:27 +0200

    Ajout login limiter grâce à RateLimit package

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const router = express.Router();
 const userCtrl = require("../controllers/user");
 
+const loginLimiter= rateLimit({
+    windoMs: 15 * 60 * 1000, // 15 minutes //
+    max : 3, // Bloque après 3 essais // 
+});
+
 router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/login", loginLimiter, userCtrl.login);
 
 module.exports = router;
